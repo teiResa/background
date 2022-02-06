e42d5bda932421feac9474464879499c11c57473
Theresa <49374955+teiResa@users.noreply.github.com>
2022-02-07 03:39:09 +0800

    More questions needed. 5/10
    maybe another set or two tomorrow?

--- quiz/quiz-ux/questions.js
+++ quiz/quiz-ux/questions.js
@@ -1,13 +1,88 @@
 // Minimum of 10 questions or it crashes the previewer.
  const questions = [
   {
-      question: "How many days makes a week ?",
-      optionA: "10 days",
-      optionB: "14 days",
-      optionC: "5 days",
-      optionD: "7 days",
-      correctOption: "optionD"
+      question: "Which does affordance not mean?",
+      optionA: "What tells you how to should use a thing.",
+      optionB: "The properties that define possible uses of an object.",
+      optionC: "The amount of resources available for the project.",
+      optionD: "The handle of a door, for example.",
+      correctOption: "optionC"
   },
+  {
+      question: "Which is a not true of a mind model?",
+      optionA: "The U's belief of how UX works.",
+      optionB: "Based on past interactions with products.",
+      optionC: "U doodles on paper while testing product.",
+      optionD: "Fully mental picture of U's expectations.",
+      correctOption: "optionC"
+  },
+  {
+      question: "Microinteractions are not...",
+      optionA: "Touch-screen gestures to help the Us.",
+      optionB: "Encourage Us to interact with the UI.",
+      optionC: "Help prevent user errors.",
+      optionD: "are the best way to users engaged.",
+      correctOption: "optionA"
+  },
+  {
+      question: "Wireframe annotation are good for...",
+      optionA: "bridging the place between LoFi and HiFi.",
+      optionB: "passing notes to colleagues and partners.",
+      optionC: "planning the classes for code monkeys.",
+      optionD: "rationale, edge cases, examples, etc.",
+      correctOption: "optionC"
+  },
+  {
+      question: "Gestute-based nav ...",
+      optionA: "allows for more screen space.",
+      optionB: "limits valuable content.",
+      optionC: "allows for improved UI design.",
+      optionD: "can be inaccessible.",
+      correctOption: "optionB"
+  },
+  {
+      question: "Wireframe annotation are good for...",
+      optionA: "bridging the place between LoFi and HiFi.",
+      optionB: "passing notes to colleagues and partners.",
+      optionC: "planning the classes for code monkeys.",
+      optionD: "rationale, edge cases, examples, etc.",
+      correctOption: "optionC"
+  },
+  {
+    question: "Wireframe annotation are good for...",
+    optionA: "bridging the place between LoFi and HiFi.",
+    optionB: "passing notes to colleagues and partners.",
+    optionC: "planning the classes for code monkeys.",
+    optionD: "rationale, edge cases, examples, etc.",
+    correctOption: "optionC"
+},
+{
+  question: "Wireframe annotation are good for...",
+  optionA: "bridging the place between LoFi and HiFi.",
+  optionB: "passing notes to colleagues and partners.",
+  optionC: "planning the classes for code monkeys.",
+  optionD: "rationale, edge cases, examples, etc.",
+  correctOption: "optionC"
+},
+{
+  question: "Wireframe annotation are good for...",
+  optionA: "bridging the place between LoFi and HiFi.",
+  optionB: "passing notes to colleagues and partners.",
+  optionC: "planning the classes for code monkeys.",
+  optionD: "rationale, edge cases, examples, etc.",
+  correctOption: "optionC"
+},
+{
+  question: "Wireframe annotation are good for...",
+  optionA: "bridging the place between LoFi and HiFi.",
+  optionB: "passing notes to colleagues and partners.",
+  optionC: "planning the classes for code monkeys.",
+  optionD: "rationale, edge cases, examples, etc.",
+  correctOption: "optionC"
+},
+  
+  
+  
 
 
   
