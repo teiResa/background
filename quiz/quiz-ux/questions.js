1b458d692aeda1882001bf9f2de385473f1cda4f
Theresa <49374955+teiResa@users.noreply.github.com>
2022-02-06 23:47:03 +0800

    quizes setup

---
+++ quiz/quiz-ux/questions.js
@@ -0,0 +1,92 @@
+//this would be the object shape for storing the questions  
+ //you can change the questions to your own taste or even add more questions..
+ const questions = [
+  {
+      question: "How many days makes a week ?",
+      optionA: "10 days",
+      optionB: "14 days",
+      optionC: "5 days",
+      optionD: "7 days",
+      correctOption: "optionD"
+  },
+
+  {
+      question: "How many players are allowed on a soccer pitch ?",
+      optionA: "10 players",
+      optionB: "11 players",
+      optionC: "9 players",
+      optionD: "12 players",
+      correctOption: "optionB"
+  },
+  {
+    question: "How many days makes a week ?",
+    optionA: "10 days",
+    optionB: "14 days",
+    optionC: "5 days",
+    optionD: "7 days",
+    correctOption: "optionD"
+},
+
+{
+    question: "How many players are allowed on a soccer pitch ?",
+    optionA: "10 players",
+    optionB: "11 players",
+    optionC: "9 players",
+    optionD: "12 players",
+    correctOption: "optionB"
+},
+{
+  question: "How many days makes a week ?",
+  optionA: "10 days",
+  optionB: "14 days",
+  optionC: "5 days",
+  optionD: "7 days",
+  correctOption: "optionD"
+},
+
+{
+  question: "How many players are allowed on a soccer pitch ?",
+  optionA: "10 players",
+  optionB: "11 players",
+  optionC: "9 players",
+  optionD: "12 players",
+  correctOption: "optionB"
+},
+{
+  question: "How many days makes a week ?",
+  optionA: "10 days",
+  optionB: "14 days",
+  optionC: "5 days",
+  optionD: "7 days",
+  correctOption: "optionD"
+},
+
+{
+  question: "How many players are allowed on a soccer pitch ?",
+  optionA: "10 players",
+  optionB: "11 players",
+  optionC: "9 players",
+  optionD: "12 players",
+  correctOption: "optionB"
+},
+{
+  question: "How many days makes a week ?",
+  optionA: "10 days",
+  optionB: "14 days",
+  optionC: "5 days",
+  optionD: "7 days",
+  correctOption: "optionD"
+},
+
+{
+  question: "How many players are allowed on a soccer pitch ?",
+  optionA: "10 players",
+  optionB: "11 players",
+  optionC: "9 players",
+  optionD: "12 players",
+  correctOption: "optionB"
+},
+
+  
+
+]
