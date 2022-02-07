b21bdc5466fe31dafed3f32f739b87f0439b7001
Theresa <49374955+teiResa@users.noreply.github.com>
2022-02-07 15:22:12 +0800

    cupcake ipsum
    - separated sample Qs from UX set
    - made  Q and Options appropriate for longer text. Pretty too.
    - removed growing (set's css overrides it)
    - corrected medium modals. Less needlessly massive.
    - made list for quizzes to be together on a GUI.

---
+++ quiz/sample-questions.js
@@ -0,0 +1,100 @@
+// Minimum of 10 questions or it crashes the previewer.
+const questions = [
+  {
+      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie.  C",
+      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
+      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
+      optionC: " Caramels croissant gingerbread.",
+      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      correctOption: "optionC"
+  },
+  {
+      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie. C",
+      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
+      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
+      optionC: " Caramels croissant gingerbread.",
+      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      correctOption: "optionC"
+  },
+  {
+      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie. C",
+      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
+      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
+      optionC: " Caramels croissant gingerbread.",
+      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      correctOption: "optionC"
+  },
+  {
+      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie. C",
+      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
+      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
+      optionC: " Caramels croissant gingerbread.",
+      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      correctOption: "optionC"
+  },
+  {
+      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie.  C",
+      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
+      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
+      optionC: " Caramels croissant gingerbread.",
+      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      correctOption: "optionC"
+  },
+  {
+      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie. . C",
+      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
+      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
+      optionC: " Caramels croissant gingerbread.",
+      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      correctOption: "optionC"
+  },
+  {
+      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie.  C",
+      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
+      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
+      optionC: " Caramels croissant gingerbread.",
+      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      correctOption: "optionC"
+  },
+  {
+      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie.  C",
+      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
+      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
+      optionC: " Caramels croissant gingerbread.",
+      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      correctOption: "optionC"
+  },
+  {
+      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie.  C",
+      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
+      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
+      optionC: " Caramels croissant gingerbread.",
+      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      correctOption: "optionC"
+  },
+  {
+      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie.  C",
+      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
+      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
+      optionC: " Caramels croissant gingerbread.",
+      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      correctOption: "optionC"
+  },
+  {
+      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie. C",
+      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
+      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
+      optionC: " Caramels croissant gingerbread.",
+      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      correctOption: "optionC"
+  },
+  {
+      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie. C",
+      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
+      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
+      optionC: " Caramels croissant gingerbread.",
+      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      correctOption: "optionC"
+  },
+
+]
