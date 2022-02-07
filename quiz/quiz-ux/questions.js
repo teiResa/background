9a53fb0db47c68813f38484904220b9ff425fd0d
Theresa <49374955+teiResa@users.noreply.github.com>
2022-02-07 20:14:37 +0800

    ux, tags
    - fixed the "span label" so the red/green fits the box.
    - UX set completed (maybe more later)
    - tags quiz set-up
     -tags added to quizzes (quizes.html)

--- quiz/quiz-ux/questions.js
+++ quiz/quiz-ux/questions.js
@@ -41,50 +41,171 @@
       correctOption: "optionB"
   },
   {
-      question: "Wireframe annotation are good for...",
-      optionA: "bridging the place between LoFi and HiFi.",
-      optionB: "passing notes to colleagues and partners.",
-      optionC: "planning the classes for code monkeys.",
-      optionD: "rationale, edge cases, examples, etc.",
+      question: "What is your drop-off rate?",
+      optionA: "However often a user posts.",
+      optionB: "How many times a user can upload from a single source.",
+      optionC: "How many times a page is loaded/refreshed.",
+      optionD: "How quickly users abandon a website due to poor performance.",
+      correctOption: "optionD"
+    },
+    {
+      question: "&quot;If you're not embarrassed by your first product release, you released too late&quot;",
+      optionA: "Hoffman's Theory",
+      optionB: "Jarred Dunn's Theory",
+      optionC: "The Berners-Lee Theory",
+      optionD: "The Kleinrock Theory",
+      correctOption: "optionA"
+  },
+  {
+      question: "What is a beta?",
+      optionA: "Something second-rate.",
+      optionB: "The second letter of the Phonetic Alphabet.",
+      optionC: "The &#x3B2; of the Cyrillic Alphabet.",
+      optionD: "EUT or UAT. Real world testing and final touches.",
+      correctOption: "optionD"
+  },
+  {
+      question: "FE vs BE. Which is most correct?,
+      optionA: "Everything the user interacts with is BE.",
+      optionB: "Everything the user interacts with is FE.",
+      optionC: "Trick question. They overlap too much to differentiate.",
+      optionD: "None/all of the above.",
       correctOption: "optionC"
   },
   {
-    question: "Wireframe annotation are good for...",
-    optionA: "bridging the place between LoFi and HiFi.",
-    optionB: "passing notes to colleagues and partners.",
-    optionC: "planning the classes for code monkeys.",
-    optionD: "rationale, edge cases, examples, etc.",
-    correctOption: "optionC"
-},
-{
-  question: "Wireframe annotation are good for...",
-  optionA: "bridging the place between LoFi and HiFi.",
-  optionB: "passing notes to colleagues and partners.",
-  optionC: "planning the classes for code monkeys.",
-  optionD: "rationale, edge cases, examples, etc.",
-  correctOption: "optionC"
-},
-{
-  question: "Wireframe annotation are good for...",
-  optionA: "bridging the place between LoFi and HiFi.",
-  optionB: "passing notes to colleagues and partners.",
-  optionC: "planning the classes for code monkeys.",
-  optionD: "rationale, edge cases, examples, etc.",
-  correctOption: "optionC"
-},
-{
-  question: "Wireframe annotation are good for...",
-  optionA: "bridging the place between LoFi and HiFi.",
-  optionB: "passing notes to colleagues and partners.",
-  optionC: "planning the classes for code monkeys.",
-  optionD: "rationale, edge cases, examples, etc.",
-  correctOption: "optionC"
-},
-  
-  
-  
+      question: "Which is most correct?",
+      optionA: "Working in a team limits creativity.",
+      optionB: "Working in a team is the only way to complete a UX.",
+      optionC: "Working in a team allows for more variety.",
+      optionD: "None/all of the above.",
+      correctOption: "optionC"
+  },
+  {
+      question: "Which is the final step of the design process?",
+      optionA: "EUT - End User Testing",
+      optionB: "ABT - A-B Testing",
+      optionC: "Beta Testing",
+      optionD: "UAT - User Acceptance Testing",
+      correctOption: "optionC"
+  },
+  {
+      question: "Which are examples of User Research methods?",
+      optionA: "Focus Groups, interviews, Personas, and having Us use existing products.",
+      optionB: "Market research, others reviews, and having others in the field test the product.",
+      optionC: "Focus Groups, emotions, eye-tracking, and having Us use existing products.",
+      optionD: "None/all of the above.",
+      correctOption: "optionC"
+  },
+  {
+      question: "A visual way of defining how the site/app will work.",
+      optionA: "User Journey",
+      optionB: "Site Map",
+      optionC: "Mind Mapping",
+      optionD: "Interactiver prototype",
+      correctOption: "optionD"
+  },
+  {
+      question: "U's experience during using session of using the app/site.",
+      optionA: "User Journey",
+      optionB: "Site Map",
+      optionC: "Mind Mapping",
+      optionD: "Interactiver prototype",
+      correctOption: "optionA"
+  },
+  {
+      question: "The series of actions performed to achieve a goal.",
+      optionA: "User Journey",
+      optionB: "Site Map",
+      optionC: "Mind Mapping",
+      optionD: "Interactiver prototype",
+      correctOption: "optionA"
+  },
+  {
+      question: "Which are examples of User Research methods?",
+      optionA: "User Journey",
+      optionB: "Site Map",
+      optionC: "Mind Mapping",
+      optionD: "Interactiver prototype",
+      correctOption: "optionC"
+  },
+  {
+      question: "The most perfectly ideal path is the _____ path(s).",
+      optionA: "Happy",
+      optionB: "Alternate",
+      optionC: "Priority",
+      optionD: "User",
+      correctOption: "optionA"
+  },
+  {
+      question: "Other possible routes the user may take are the _____ path(s).",
+      optionA: "Happy",
+      optionB: "Alternate",
+      optionC: "Priority",
+      optionD: "User",
+      correctOption: "optionB"
+  },
+  {
+      question: "The routes the user can take are _____ path(s).",
+      optionA: "Happy",
+      optionB: "Alternate",
+      optionC: "Priority",
+      optionD: "User",
+      correctOption: "optionB"
+  },
+  {
+      question: "The routes you want the user to take are the _____ path(s).",
+      optionA: "Happy",
+      optionB: "Alternate",
+      optionC: "Priority",
+      optionD: "User",
+      correctOption: "optionA"
+  },
+  {
+      question: "LoFi or HiFi? Has more functionality.",
+      optionA: "LoFi",
+      optionB: "HiFi",
+      optionC: "neither",
+      optionD: "both",
+      correctOption: "optionA"
+  },
+  {
+      question: "LoFi or HiFi? Simple, low-tech concepts.",
+      optionA: "LoFi",
+      optionB: "HiFi",
+      optionC: "neither",
+      optionD: "both",
+      correctOption: "optionA"
+  },
+  {
+      question: "LoFi or HiFi? Hand drawn or digitally mocked up.",
+      optionA: "LoFi",
+      optionB: "HiFi",
+      optionC: "neither",
+      optionD: "both",
+      correctOption: "optionB"
+  },
+  {
+      question: "LoFi or HiFi? Has more visual aspects.",
+      optionA: "LoFi",
+      optionB: "HiFi",
+      optionC: "neither",
+      optionD: "both",
+      correctOption: "optionB"
+  },
+  {
+      question: "LoFi or HiFi? The complete and final website.",
+      optionA: "LoFi",
+      optionB: "HiFi",
+      optionC: "neither",
+      optionD: "both",
+      correctOption: "optionC"
+  },
+
+          
+      
+    
 
 
-  
+    
 
 ]
--- quiz/sample-questions.js
+++ quiz/sample-questions.js
@@ -2,99 +2,84 @@
 const questions = [
   {
       question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie.  C",
-      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
-      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
-      optionC: " Caramels croissant gingerbread.",
-      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
-      correctOption: "optionC"
-  },
-  {
-      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie. C",
-      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
-      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
-      optionC: " Caramels croissant gingerbread.",
-      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
-      correctOption: "optionC"
-  },
-  {
-      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie. C",
-      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
-      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
-      optionC: " Caramels croissant gingerbread.",
-      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
-      correctOption: "optionC"
-  },
-  {
-      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie. C",
-      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
-      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
-      optionC: " Caramels croissant gingerbread.",
-      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      optionA: "Gummies",
+      optionB: "Halvah",
+      optionC: "Gingerbread",
+      optionD: "Lemon Drops",
       correctOption: "optionC"
   },
   {
       question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie.  C",
-      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
-      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
-      optionC: " Caramels croissant gingerbread.",
-      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      optionA: "Gummies",
+      optionB: "Halvah",
+      optionC: "Gingerbread",
+      optionD: "Lemon Drops",
       correctOption: "optionC"
   },
-  {
-      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie. . C",
-      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
-      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
-      optionC: " Caramels croissant gingerbread.",
-      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+    {
+      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie.  C",
+      optionA: "Gummies",
+      optionB: "Halvah",
+      optionC: "Gingerbread",
+      optionD: "Lemon Drops",
       correctOption: "optionC"
   },
   {
       question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie.  C",
-      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
-      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
-      optionC: " Caramels croissant gingerbread.",
-      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      optionA: "Gummies",
+      optionB: "Halvah",
+      optionC: "Gingerbread",
+      optionD: "Lemon Drops",
       correctOption: "optionC"
   },
   {
       question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie.  C",
-      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
-      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
-      optionC: " Caramels croissant gingerbread.",
-      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      optionA: "Gummies",
+      optionB: "Halvah",
+      optionC: "Gingerbread",
+      optionD: "Lemon Drops",
       correctOption: "optionC"
   },
   {
       question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie.  C",
-      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
-      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
-      optionC: " Caramels croissant gingerbread.",
-      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      optionA: "Gummies",
+      optionB: "Halvah",
+      optionC: "Gingerbread",
+      optionD: "Lemon Drops",
       correctOption: "optionC"
   },
   {
       question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie.  C",
-      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
-      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
-      optionC: " Caramels croissant gingerbread.",
-      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      optionA: "Gummies",
+      optionB: "Halvah",
+      optionC: "Gingerbread",
+      optionD: "Lemon Drops",
       correctOption: "optionC"
   },
   {
-      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie. C",
-      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
-      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
-      optionC: " Caramels croissant gingerbread.",
-      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
+      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie.  C",
+      optionA: "Gummies",
+      optionB: "Halvah",
+      optionC: "Gingerbread",
+      optionD: "Lemon Drops",
       correctOption: "optionC"
   },
   {
-      question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie. C",
-      optionA: "Ice cream cheesecake sweet jujubes muffin candy canes topping candy.",
-      optionB: "Gummies jujubes cheesecake I love marshmallow powder cookie.",
-      optionC: " Caramels croissant gingerbread.",
-      optionD: "chocolate cake topping lemon drops icing cotton candy apple pie halvah.",
-      correctOption: "optionC"
+        question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie.  C",
+        optionA: "Gummies",
+        optionB: "Halvah",
+        optionC: "Gingerbread",
+        optionD: "Lemon Drops",
+        correctOption: "optionC"
+  },
+  {
+        question: " Dragée cake cupcake pudding sweet candy dragée. Chupa chups icing sweet tiramisu oat cake cake wafer. Cotton candy croissant ice cream jelly beans pie danish topping sugar plum apple pie.  C",
+        optionA: "Gummies",
+        optionB: "Halvah",
+        optionC: "Gingerbread",
+        optionD: "Lemon Drops",
+        correctOption: "optionC"
   },
 
-]
+
+  ]   //end of quiz questions
