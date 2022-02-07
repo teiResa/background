34f9ad98be7f985cdeb999369267783224048bb1
Theresa <49374955+teiResa@users.noreply.github.com>
2022-02-08 02:54:51 +0800

    started html tags quiz

---
+++ quiz/quiz-html-tags/questions-html-tags.js
@@ -0,0 +1,83 @@
+// Minimum of 10 questions or it crashes the previewer.
+// only one js file containing "questions" can be open at once.
+const questions = [
+  {
+      question: "Which attributes are requires for an input tag to ",
+      optionA: "What tells you how to should use a thing.",
+      optionB: "The properties that define possible uses of an object.",
+      optionC: "The amount of resources available for the project.",
+      optionD: "The handle of a door, for example.",
+      correctOption: "optionC"
+  },
+  {
+    question: "html",
+    optionA: "What tells you how to should use a thing.",
+    optionB: "The properties that define possible uses of an object.",
+    optionC: "The amount of resources available for the project.",
+    optionD: "The handle of a door, for example.",
+    correctOption: "optionC"
+},  {
+  question: "html",
+  optionA: "What tells you how to should use a thing.",
+  optionB: "The properties that define possible uses of an object.",
+  optionC: "The amount of resources available for the project.",
+  optionD: "The handle of a door, for example.",
+  correctOption: "optionC"
+},  {
+  question: "html",
+  optionA: "What tells you how to should use a thing.",
+  optionB: "The properties that define possible uses of an object.",
+  optionC: "The amount of resources available for the project.",
+  optionD: "The handle of a door, for example.",
+  correctOption: "optionC"
+},  {
+  question: "html",
+  optionA: "What tells you how to should use a thing.",
+  optionB: "The properties that define possible uses of an object.",
+  optionC: "The amount of resources available for the project.",
+  optionD: "The handle of a door, for example.",
+  correctOption: "optionC"
+},  {
+  question: "html",
+  optionA: "What tells you how to should use a thing.",
+  optionB: "The properties that define possible uses of an object.",
+  optionC: "The amount of resources available for the project.",
+  optionD: "The handle of a door, for example.",
+  correctOption: "optionC"
+},  {
+  question: "html",
+  optionA: "What tells you how to should use a thing.",
+  optionB: "The properties that define possible uses of an object.",
+  optionC: "The amount of resources available for the project.",
+  optionD: "The handle of a door, for example.",
+  correctOption: "optionC"
+},  {
+  question: "html",
+  optionA: "What tells you how to should use a thing.",
+  optionB: "The properties that define possible uses of an object.",
+  optionC: "The amount of resources available for the project.",
+  optionD: "The handle of a door, for example.",
+  correctOption: "optionC"
+},  {
+  question: "html",
+  optionA: "What tells you how to should use a thing.",
+  optionB: "The properties that define possible uses of an object.",
+  optionC: "The amount of resources available for the project.",
+  optionD: "The handle of a door, for example.",
+  correctOption: "optionC"
+},  {
+  question: "html",
+  optionA: "What tells you how to should use a thing.",
+  optionB: "The properties that define possible uses of an object.",
+  optionC: "The amount of resources available for the project.",
+  optionD: "The handle of a door, for example.",
+  correctOption: "optionC"
+},  {
+  question: "html",
+  optionA: "What tells you how to should use a thing.",
+  optionB: "The properties that define possible uses of an object.",
+  optionC: "The amount of resources available for the project.",
+  optionD: "The handle of a door, for example.",
+  correctOption: "optionC"
+},
+]
