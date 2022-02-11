9001c011bb08c4c6e088e905fa52ac2f4906bb68
Theresa <49374955+teiResa@users.noreply.github.com>
2022-02-11 21:46:39 +0800

    Update time.js

--- current-time/assets/time.js
+++ current-time/assets/time.js
@@ -16,11 +16,13 @@ setInterval(function(){
       var today = new Date();
       var day = today.getDay();
       var daylist = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+      // ("0" + (today.getMonth()+1)) makes it display this month, not the month before this current month.
       var date = today.getFullYear()+' - '+ ("0" + (today.getMonth()+1)).slice(-2) +' - '+ ("0" + today.getDate()).slice(-2);
-
+      // today.get____()).slice(-2) forces it to show both digits. Without this it was just showing one place if <10 .
       var time = ("0" + today.getHours()).slice(-2) + ":" + ("0" + today.getMinutes()).slice(-2) + ":" + ("0" + today.getSeconds()).slice(-2)
       document.getElementById('curr_time').innerHTML = time;
       var dateTime = date+' at '+time;
+      // daylist[day] gets today from the daylist, then represents it how it is written in the daylist list.
       document.getElementById("curr_time").innerHTML = 'For me it is currently ' + daylist[day] + ' ' + dateTime ;
 
       i--;
