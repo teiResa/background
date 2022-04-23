6d745b44ea54b04f0c6e7df1ca4d96d9753863bc
Theresa <49374955+teiResa@users.noreply.github.com>
2022-04-23 23:36:01 +0800

    french verbs quiz created

---
+++ quiz/fr/verbs/questions-fr-verbs.js
@@ -0,0 +1,139 @@
+// Minimum of 10 questions or it crashes the previewer.
+// only one js file containing "questions" can be open at once.
+const questions = [
+ {
+     question: "Devenir",
+     optionA: "To become",
+     optionB: "To come back",
+     optionC: "To leave",
+     optionD: "To arrive",
+     correctOption: "optionA"
+ },
+ {
+  question: "Revenir",
+  optionA: "To become",
+  optionB: "To come back",
+  optionC: "To leave",
+  optionD: "To arrive",
+  correctOption: "optionB"
+},  {
+ question: "Partir",
+ optionA: "To become",
+ optionB: "To come back",
+ optionC: "To leave",
+ optionD: "To arrive",
+ correctOption: "optionC"
+},  {
+ question: "Arriver",
+ optionA: "To become",
+ optionB: "To come back",
+ optionC: "To leave",
+ optionD: "To arrive",
+ correctOption: "optionD"
+},  {
+ question: "Monter",
+ optionA: "To go up",
+ optionB: "To stay",
+ optionC: "To go down",
+ optionD: "To die",
+ correctOption: "optionA"
+},  {
+ question: "Rester",
+ optionA: "To go up",
+ optionB: "To stay",
+ optionC: "To go down",
+ optionD: "To die",
+ correctOption: "optionB"
+},  {
+ question: "Descendre",
+ optionA: "To go up",
+ optionB: "To stay",
+ optionC: "To go down",
+ optionD: "To die",
+ correctOption: "optionC"
+},  {
+ question: "Mourir",
+ optionA: "To go up",
+ optionB: "To rest",
+ optionC: "To go down",
+ optionD: "To die",
+ correctOption: "optionD"
+},  {
+ question: "Sortir",
+ optionA: "To go out",
+ optionB: "To come",
+ optionC: "To go home",
+ optionD: "To go back",
+ correctOption: "optionA"
+},  {
+ question: "Venir",
+ optionA: "To go out",
+ optionB: "To come",
+ optionC: "To go home",
+ optionD: "To go back",
+ correctOption: "optionB"
+},  {
+ question: "Rentrer",
+ optionA: "To go out",
+ optionB: "To come",
+ optionC: "To go home",
+ optionD: "To go back",
+ correctOption: "optionC"
+},
+{
+ question: "Retourner",
+ optionA: "To go out",
+ optionB: "To come",
+ optionC: "To go home",
+ optionD: "To go back",
+ correctOption: "optionD"
+},
+{
+ question: "Naïtre",
+ optionA: "To be born",
+ optionB: "To enter",
+ optionC: "To come",
+ optionD: "To pass by",
+ correctOption: "optionA"
+},
+{
+ question: "Entrer",
+ optionA: "To be born",
+ optionB: "To enter",
+ optionC: "To come",
+ optionD: "To pass by",
+ correctOption: "optionB"
+},
+{
+ question: "Venir",
+ optionA: "To be born",
+ optionB: "To enter",
+ optionC: "To come",
+ optionD: "To pass by",
+ correctOption: "optionC"
+},
+{
+ question: "Passer",
+ optionA: "To be born",
+ optionB: "To enter",
+ optionC: "To come",
+ optionD: "To pass by",
+ correctOption: "optionD"
+},
+{
+ question: "Tomber",
+ optionA: "To fall (to fall down)",
+ optionB: "To enter",
+ optionC: "To go",
+ optionD: "To pass by",
+ correctOption: "optionA"
+},
+{
+ question: "Tomber",
+ optionA: "To fall (to fall down)",
+ optionB: "To enter",
+ optionC: "To go",
+ optionD: "To pass by",
+ correctOption: "optionC"
+},
+]
