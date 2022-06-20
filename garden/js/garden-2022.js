a6469bd912a3e7f553f044ee8104fe2143419305
Theresa <49374955+teiResa@users.noreply.github.com>
2022-06-20 19:02:33 +0800

    garden table
    refer to txt pg 535, 537

---
+++ garden/js/garden-2022.js
@@ -0,0 +1,48 @@
+var garden = [
+ Weinmannifolia = {
+ name: 'fragrant tree',
+ latinName: 'Pistacia Weinmannifolia',
+ pinyinName: 'Qing Xiang Mu',
+ chinaName: '青香木',
+ type: 'bonsai',  //herb, food, bonsai, etc.
+ pH: null,
+ sun: 'full, but not directly in HZ sun',
+ water: '150 to 200ml 3-4X /week',
+ feed: 'every few weeks',
+ container: 'clay pot with drainage holes',
+ petSafe: false,   //false by default
+ foodSafe: true,   //used in TCM
+ note: 'water when soil feels dry to touch'
+},
+Catnip = {
+ name: 'Catnip',
+ latinName: 'Nepeta Cataria',
+ pinyinName: 'māo bò he',
+ chinaName: '猫薄荷',
+ type: 'herb / perennial',  //herb, food, bonsai, etc.
+ pH: null,
+ sun: 'full, but not directly in HZ sun',
+ water: 'drought-tolerant',
+ feed: 'every month or so',
+ container: '12in diameter, clay with drainage hole',
+ prune: 'prune off the flowers as they\'re starting to degrade and before they go to seed.  Pinch at 6-8in growth to encourage bushier growth. After first frost in the fall, cut back mature plants to just a few inches from the soil. They will regenerate in the spring with fresh growth. ',
+ petSafe: true,   //false by default
+ foodSafe: true,   //used in TCM
+ note: 'water when soil feels dry to touch. Sedative and digestive tea.'
+},
+ScotchBonnet = {
+ name: 'Scotch Bonnet',
+ latinName: 'Capsicum Chinense',
+ pinyinName: 'n/a',
+ chinaName: 'n/a',
+ type: 'pepper',  //herb, food, bonsai, etc.
+ pH: null,
+ sun: 'full, but not directly in HZ sun',
+ water: '150 to 200ml 3-4X /week',
+ feed: 'prefruit= Normal strength nitrogen-rich nutrients fertiliser every few weeks. Fruiting =  higher potassium and phosphorus. ',
+ container: 'clay pot with drainage holes',
+ petSafe: false,   //false by default
+ foodSafe: true,   //used in TCM
+ note: 'water when soil feels dry to touch'
+}
+];
