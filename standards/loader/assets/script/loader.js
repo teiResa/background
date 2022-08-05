ad1fe593157524bcd4e58e1a17462c0e2def15ba
Theresa <49374955+teiResa@users.noreply.github.com>
2022-08-05 18:15:32 +0800

    MOVED loader animation

---
+++ standards/loader/assets/script/loader.js
@@ -0,0 +1,13 @@
+{/* <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+<!-- loader animation's JS -->
+<script src="https://teiresa.github.io/background/standards/loader/assets/script/loader.js"></script> */}
+
+
+
+  // jQuery for loading animation 
+    $(window).load(function() {
+    $(".preloader").delay(5000).fadeOut("slow");
+    $("#overlayer").delay(5000).fadeOut("slow");
+    })
+ 
