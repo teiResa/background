01cf160afed739b50eedf5e5c9fa42a10badf407
Theresa <49374955+teiResa@users.noreply.github.com>
2023-02-05 19:43:56 +0800

    Planning

--- rps/scripts.js
+++ rps/scripts.js
@@ -1,2 +1,2 @@
-// IDEA: if user holds space bar while selecting their gesture, the computer should choose the gesture which loses.
+// IDEA: if user holds space bar while selecting their gesture, the computer should choose the gesture which loses. Or maybe a sticky keys like code? Tap L 5 times to activate babying mode.
 
